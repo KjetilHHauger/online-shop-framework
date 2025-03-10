1618a4fa87397f64c7eda78751b8abd0dbbb5ae2
Kjetil Harneshaug Hauger <kjehar01278@stud.noroff.no>
2025-03-10 20:15:24 +0100

    Update CartPage.jsx

--- src/routes/CartPage.jsx
+++ src/routes/CartPage.jsx
@@ -34,14 +34,14 @@ function Cart() {
   );
 
   return (
-    <div className="max-w-3xl mx-auto p-4">
+    <div className="max-w-3xl mx-auto p-2">
       <h1 className="text-3xl font-bold mb-4">Your Cart</h1>
       {items.length > 0 ? (
         <div className="flex flex-col gap-4">
           {items.map((item) => (
             <div
               key={item.id}
-              className="mb-4 p-4 border rounded flex items-center justify-between gap-8"
+              className="mb-4 p-4 border rounded flex flex-col items-start justify-between gap-2 md:flex-row"
             >
               <div>
                 <h2 className="text-xl font-semibold">{item.title}</h2>
